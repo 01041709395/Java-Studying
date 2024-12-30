7f954d6df00feead4c632a9df2d33f13bc82dabb
Seok gi.Hong <162648332+01041709395@users.noreply.github.com>
2024-12-30 21:49:22 +0900

    반복문 별 50개 만들기

---
+++ FlowEx14.java
@@ -0,0 +1,7 @@
+public class FlowEx14 {
+    public static void main(String[] args) {
+        for (int i=1,j=10; i<=10; i++,j--) { // i와 j 두개의 변수를 1과 10으로 초기화 시키고 동시에 1씩 증가하고 감소시키는 반복문
+            System.out.printf("%d \t %d%n", i, j);
+        }
+    }
+}
