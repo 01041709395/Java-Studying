4155d84f9c50592d840872ffa18c937465a7200c
Seok gi.Hong <162648332+01041709395@users.noreply.github.com>
2025-01-02 18:42:03 +0900

    while - go back

---
+++ FlowEx23.java
@@ -0,0 +1,9 @@
+public class FlowEx23 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while(i--!=0) {
+            System.out.println(i + " - 예솜아 사랑해♥");
+        }
+    }
+}
