9a59d714a36a8f566161beb51a89e64d1054ef49
Seok gi.Hong <162648332+01041709395@users.noreply.github.com>
2025-01-04 11:49:10 +0900

    do while : 369 게임

---
+++ FlowEx29.java
@@ -0,0 +1,16 @@
+public class FlowEx29 {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++) {
+            System.out.printf("i=%d",i);
+
+            int tmp = i; // i의 값을 다른 변수에 저장
+
+            do {
+                if(tmp%10%3==0 && tmp%10!=0) //tmp%10이 3의 배수인지 확인(0은 제외)
+                    System.out.print("짝");
+            } while ((tmp/=10) !=0); //tmp /= 10은 tmp = tmp / 10과 동일
+
+            System.out.println();
+        }
+    }
+}
